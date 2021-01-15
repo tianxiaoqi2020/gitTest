566180bf4e0945ff25dd587062778b61bcaa926a
TianQJ <TianQJ@atguigu.com>
2021-01-15 10:08:53 +0800

    first commit

---
+++ src/main/java/com/jinqitian/git/hellGit.java
@@ -0,0 +1,14 @@
+package com.jinqitian.git;
+
+/**
+ * @ClassName:hellGit
+ * @Description:
+ * @Author: tqj  Email:1772490560@qq.com
+ * @Date:2021/1/14 11:57
+ * @Virsion:
+ */
+public class hellGit {
+    public static void main(String[] args) {
+        System.out.println("我命由我，不由天！");
+    }
+}
